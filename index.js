ac3b1463cb70972dd7c25412153f25c5b3b53aeb
Florent Galland <florent.galland@gmail.com>
2016-06-08 22:34:52 +0200

    alternative body parser

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+function bodyParser(req, res, next) {
+    if (!req.is('application/logplex-1')) {
+        res.send(500, 'invalid');
+        return;
+    }
+
+    req.logplexLogs = '';
+    req.setEncoding('utf8');
+    req.on('data', function (chunk) {
+        req.logplexLogs += chunk;
+    });
+    req.on('end', next);
+}
+
+app.use(bodyParser);
 
 app.post('/drain/fet7jr4ho98tf3', function (req, res) {
-    console.log(req.body);
+    console.log(req.logplexLogs);
     return res.send(200);
 });
 
